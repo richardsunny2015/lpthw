2162c095dc8a78790d19e12d0b05cacb95720025
richardsunny2015 <richardsunny2015@gmail.com>
2019-01-04 14:34:13 -0500

    feat(ex11): Complete exercise 11

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end= ' ')
+age = input()
+print("How tall are you?", end= ' ')
+height = input()
+print("How much do you weigh?", end= ' ')
+weight = input()
+home = input("Where are you from? ")
+
+print(f"So, you're {age} old, from {home}, {height} tall and {weight} heavy.")
