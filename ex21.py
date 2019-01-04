6200fd8366ad3023d421c930a67a3221469246ef
richardsunny2015 <richardsunny2015@gmail.com>
2019-01-04 17:35:37 -0500

    feat(ex21): Complete exercise twenty-one

---
+++ ex21.py
@@ -0,0 +1,39 @@
+def add(a, b):
+    print(f"ADDING {a} + {b}")
+    return a + b
+
+def subtract(a, b):
+    print(f"SUBTRACTING {a} - {b}")
+    return a - b
+
+def multiply(a, b):
+    print(f"MULTIPLYING {a} * {b}")
+    return a * b
+
+def divide (a, b):
+    print(f"DIVIDING {a} / {b}")
+    return a / b
+
+print("Let's do some math with just functions!")
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+print(f"Age: {age}, Height: {height}, Weight: {weight}, IQ: {iq}")
+
+
+# A puzzle for the extra credit, type it in anyway.
+print("Here is a puzzle.")
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print("That becomes: ", what, "Can you do it by hand?")
+
+def add_divide_subtract(a, b, c, d):
+    print(f"{a} + {b} / {c} - {d}")
+    return subtract(add(a, divide(b, c)), d)
+
+result = add_divide_subtract(24, 34, 100, 1023)
+print(f"That becomes {result}")
