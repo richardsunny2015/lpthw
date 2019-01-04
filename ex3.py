5cbffbc269777c45d3b9b87a31ac2f3b80c72805
richardsunny2015 <richardsunny2015@gmail.com>
2019-01-04 12:07:35 -0500

    feat(ex3): Finish exercise 3

---
+++ ex3.py
@@ -0,0 +1,27 @@
+
+print("I will now count my chickens:")
+# Divides 30 by 6 then adds 25
+print("Hens", 25 + 30 / 6)
+# Multiplies 25 by 3 then mods the result by 4 and subtracts that number from 100 
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs:")
+# Divies first, then mods, then computes from left to right
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("I it true that 3 + 2 < 5 - 7?")
+# Computes both sides of less than symbol and compares results
+print(3 + 2 < 5 - 7)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= 2)
+print("Is it less or equal?", 5 <= -2)
+
+print("3 % 4 is:", 3 % 4)
